3ef14276f4871a4cb56ba3aad365b5a9e89dd8f6
tora <xxxxxxxx@gmail.com>
2024-01-30 22:03:16 +0900

    クレジットを追加

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,6 +8,12 @@ export default function Footer() {
       <Typography variant="caption">
         このサービスは、政府統計総合窓口(e-Stat)のAPI機能を使用していますが、サービスの内容は国によって保証されたものではありません。
       </Typography>
+      <br />
+      <Typography variant="caption">
+        <a href="https://developer.yahoo.co.jp/sitemap/">
+          Web Services by Yahoo! JAPAN
+        </a>
+      </Typography>
     </Box>
   );
 }
